780b7e269e8b87a11ba567e9b6a83b964f0a48d0
bjvanbemmel <beauvanbemmel@live.nl>
2025-09-18 18:51:02 +0200

    Add help command

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("API")
+	fmt.Println("API")
 }
--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/bjvanbemmel/go-templ/cmd/cli/commands"
+)
 
 func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		commands.Help{}.Execute()
+		return
+	}
+
+	switch args[0] {
+	case "help":
+		commands.Help{}.Execute()
+		return
+	}
+
 	fmt.Println("Hello, World!")
 }
